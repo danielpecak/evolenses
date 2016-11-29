b91372a5d363878adb4b00e7a51b3fd5169e0d89
gronki <gronki@gmail.com>
2016-11-29 23:46:16 +0100

    Dodałem pliki

---
+++ constants.F90
@@ -0,0 +1,3 @@
+module constants
+    use iso_fortran_env
+end module
---
+++ optsolv.F90
@@ -0,0 +1,11 @@
+module optsolv
+
+    use iso_fortran_env
+
+contains
+
+    subroutine lens_mx (  )
+
+    end subroutine
+
+end module
