03d7746427903fa10e62bfad49fb5b9c5663f9f6
Unknown <daniel.pecak@gmail.com>
2016-11-30 01:10:04 +0100

    Drugi moment

--- functions.F90
+++ functions.F90
@@ -1,6 +1,13 @@
 module functions
+use constants
 
 contains
-  
+  subroutine secondMoment(vecX,result)
+    real(dp), intent(in) :: vecX(:)
+    real(dp), intent(out):: result
+    real(dp) :: mu
+    mu = sum(vecX)
+    result = sum(vecX(:)**2)-mu**2
+  end subroutine secondMoment
 
-end module
+end module functions
