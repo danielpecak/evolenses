bf778016cd9969a81f1aad49f44a74953f11894c
gronki <gronki@gmail.com>
2016-12-10 16:15:37 +0100

    Dodałem procedure na obliczanie położenia brzegu soczewki

--- evolenses.F90
+++ evolenses.F90
@@ -1,17 +1,38 @@
 program evolenses
 
-  use functions
+    use functions
 
-  integer, parameter :: dp = selected_real_kind(REALPRECISION)
+    integer, parameter :: dp = selected_real_kind(REALPRECISION)
 
-  real(dp), parameter :: n_air = 1
-  real(dp), parameter :: n_glass = 1.33
+    type optsurface
+      real(dp) :: xmid, rad, diam
+      real(dp) :: xcurv, xrim
+    end type
 
-  real(dp) :: vecX(1:10)
-  real(dp) :: result
 
-  vecX=0
-  call secondMoment(vecX,result)
-  print *, result
+    real(dp), parameter :: n_air = 1
+    real(dp), parameter :: n_glass = 1.33
+
+    real(dp) :: vecX(1:10)
+    real(dp) :: result
+
+    vecX=0
+    call secondMoment(vecX,result)
+    print *, result
+
+contains
+
+    subroutine optsurf_geom(s)
+        type(optsurface), intent(inout) :: S
+        real(dp) :: c
+        if ( abs(S%rad) < (S%diam / 2) )  error stop "Lens is no good."
+        S%xcurv = ( S%xmid + S%rad )
+        c = sqrt( S%rad**2 - S%diam**2 / 4 )
+        if ( S%rad > 0 ) then
+            S%xrim = S%xcurv - c
+        else
+            S%xrim = S%xcurv + c
+        endif
+    end subroutine
 
 end program
